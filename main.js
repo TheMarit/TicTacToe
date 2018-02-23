35778d97d8c7fa8ba8a718ea5e6bf32e7175d8b8
Marit Oliemans <maritoliemans@hotmail.com>
2018-02-22 17:24:46 -0700

    Add difficulty levels, switch playernot working yet

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
 var game = {
 	player: "x",
-	Xturn: true,
-	Oturn: false,
 	usedCells: [],
 	usedCellsX: [],
 	usedCellsO: [],
 	openCells: ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
-	ongoing: true
+	ongoing: true,
+	winOptions: [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"], ["1", "4", "7"], ["2", "5", "8"], ["3", "6", "9"], ["1", "5", "9"], ["3", "5", "7"]],
+	level: [computersTurnImpossible, computersTurnHard ,computersTurnEasy],
+	difficulty: "Impossible"
 }
-var winOptions = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"], ["1", "4", "7"], ["2", "5", "8"], ["3", "6", "9"], ["1", "5", "9"], ["3", "5", "7"]];
 
 $(document).on("click", ".playagain", function(){
 	game.usedCells = [];
 	game.usedCellsX = [];
 	game.usedCellsO = [];
 	game.openCells = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-	$(".ticTacToeCell").html("");
+	$(".ticTacToeCell img").removeAttr("style");
 	game.ongoing = true;
 });
 
 $(document).on("click", ".ticTacToeCell", function(){
 	//If the clicked cell is not used, add X to cell, put in used Cells array, remove from openCells.
 	if(game.usedCells.indexOf($(this).attr('id')) == -1 && game.ongoing){
-		$(this).html('<img src="x.svg" alt="x"></div>');
+		$(this).find(".x").css("opacity","1");
 		game.usedCells.push($(this).attr('id'));
 		game.usedCellsX.push($(this).attr('id'));
 		game.openCells.splice(game.openCells.indexOf($(this).attr('id')), 1);
@@ -32,7 +32,16 @@ $(document).on("click", ".ticTacToeCell", function(){
 				return;
 			}
 		}
-		computersTurn();
+		if(game.difficulty === "Impossible"){
+			game.level[0]();
+		}
+		if(game.difficulty === "Hard"){
+			game.level[1]();
+		}
+		if(game.difficulty === "Easy"){
+			game.level[2]();
+		}
+		
 	}
 })
 
@@ -48,150 +57,21 @@ function findNumberInArrayO(currentValue) {
 function pickRandomNumber(num){
 	return Math.floor((Math.random() * num));
 }
-// ******* RANDOM COMPUTER EASY *************
-// function computersTurn(){
-// 	randomCell = game.openCells.splice(pickRandomNumber(game.openCells.length), 1)[0];
-// 	$("#" + randomCell).html('<img src="o.svg" alt="o"></div>');
-// 	game.usedCells.push(randomCell);
-// 	game.usedCellsO.push(randomCell);
-// 	if(game.usedCellsO.length >= 3){
-// 		if(checkForWinO()){
-// 			setTimeout(function(){alert("You Lose!");}, 10)
-// 			return;
-// 		}
-// 	}
-// }
+
 function checkAvailability(arr, val) {
   return arr.some(function(arrVal) {
     return val === arrVal;
   });
 }
 
-function computersTurn(){
-	var pickedCell;
-	if(game.usedCells.length === 9){
-		setTimeout(function(){alert("It's a tie!");}, 10);
-		return
-	}
-	if(game.usedCellsX.length === 1){
-		var firstXmove = game.usedCellsX[0];
-		if( firstXmove === "5"){
-			var options = ["1", "3", "7", "9"];
-			pickedCell = options.splice(pickRandomNumber(options.length), 1)[0];
-		}
-		if(firstXmove === "1" || firstXmove === "3" || firstXmove === "7" || firstXmove === "9"){
-			pickedCell = "5";
-		}
-		if(firstXmove === "2" || firstXmove === "4" || firstXmove === "6" || firstXmove === "8"){
-			var options = [];
-			for (var i=0; i < 8; i++){
-				if(checkAvailability(winOptions[i], firstXmove)){
-					for(var j=0; j < 3; j++){
-						if(winOptions[i][j] !== firstXmove){
-							options.push(winOptions[i][j]);
-						}
-					}
-				}
-			}
-			pickedCell = options.splice(pickRandomNumber(options.length), 1)[0];
-		}
-	}
-	if(game.usedCellsX.length >= 2){
-		var pairs = findPairs(game.usedCellsX);
-		for(var j=0; j< pairs.length; j++){
-			for (var i=0; i < 8; i++){
-				if(checkAvailability(winOptions[i], pairs[j][0]) && checkAvailability(winOptions[i], pairs[j][1])){
-					for(var k=0; k < 3; k++){
-						if(winOptions[i][k] !== pairs[j][0] && winOptions[i][k] !== pairs[j][1]){
-							if(checkAvailability(game.openCells, winOptions[i][k])){
-								pickedCell = winOptions[i][k];
-							}
-						}
-					}
-				}
-			}
-		}
-		if(!pickedCell){
-			var options = [];
-			var options2 = [];
-			if(game.usedCellsO.length === 1){
-				var moveO = game.usedCellsO[0];
-					for(var i=0; i< 6; i++){
-						if(checkAvailability(winOptions[i], moveO)){
-							options.push(winOptions[i]);		
-						}
-					}
-				for(var i=0; i< options.length; i++){
-					for(var j=0; j< game.usedCellsX.length; j++){
-						if(checkAvailability(options[i], game.usedCellsX[j])){
-							options.splice(i,1);
-						}
-					}
-				}
-				for(var i=0; i<options.length; i++){
-					for(var j=0; j<options[i].length; j++){
-						if(options[i][j] !== moveO){
-							options2.push(options[i][j])
-						}
-					}
-				}
-				pickedCell = options2[pickRandomNumber(options2.length)];
-			}
-			if(!pickedCell){
-			var options = [];
-			for (var k = 0; k < game.usedCellsX.length; k++){
-				for (var h = 0; h < game.usedCellsO.length ; h++){
-					for (var i=0; i < 8; i++){
-						if(checkAvailability(winOptions[i], game.usedCellsX[k]) && !checkAvailability(winOptions[i], game.usedCellsO[h])){
-							for(var j=0; j < 3; j++){
-								if(winOptions[i][j] !== game.usedCellsX[k]){
-									if(checkAvailability(game.openCells , winOptions[i][j])){
-										options.push(winOptions[i][j]);
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-			options.sort()
-			for(var l = 0; l< options.length; l++){
-				if (options[l] === options[l + 1]){
-					pickedCell = options[l];
-				}
-			}
-		}
-
-
-		}
-
-		// if(!pickedCell){
-		// 	pickedCell = game.openCells[pickRandomNumber(game.openCells.length)];
-		// 	console.log("picked random cell = " + pickedCell);
-		// }
-	}
-	if(game.usedCellsO.length >= 2){
-		var pairs = findPairs(game.usedCellsO);
-		for(var j=0; j< pairs.length; j++){
-			for (var i=0; i < 8; i++){
-				if(checkAvailability(winOptions[i], pairs[j][0]) && checkAvailability(winOptions[i], pairs[j][1])){
-					for(var k=0; k < 3; k++){
-						if(winOptions[i][k] !== pairs[j][0] && winOptions[i][k] !== pairs[j][1]){
-							if(checkAvailability(game.openCells, winOptions[i][k])){
-								pickedCell = winOptions[i][k];
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-	game.openCells.splice(game.openCells.indexOf(pickedCell), 1);
+function makeComputerMove(cell){
+	// game.openCells.splice(game.openCells.indexOf(pickedCell), 1);
 	setTimeout(function(){
-		$("#" + pickedCell).html('<img src="o.svg" alt="o"></div>');
+		$("#" + cell).find(".o").css("opacity","1");
 	}, 500)
-	game.usedCells.push(pickedCell);
-	game.usedCellsO.push(pickedCell);
+	game.usedCells.push(cell);
+	game.usedCellsO.push(cell);
+	// CHECK IF O WON
 	if(game.usedCellsO.length >= 3){
 		if(checkForWinO()){
 			setTimeout(function(){alert("You Lose!");game.ongoing = false;}, 510)
@@ -200,10 +80,9 @@ function computersTurn(){
 	}
 }
 
-
 function checkForWinX(){
 	for(var i = 0; i < 8; i++){
-		if(winOptions[i].every(findNumberInArrayX)){
+		if(game.winOptions[i].every(findNumberInArrayX)){
 			return true;
 		}
 	}
@@ -211,7 +90,7 @@ function checkForWinX(){
 
 function checkForWinO(){
 	for(var i = 0; i < 8; i++){
-		if(winOptions[i].every(findNumberInArrayO)){
+		if(game.winOptions[i].every(findNumberInArrayO)){
 			return true;
 		}
 	}
@@ -229,3 +108,219 @@ function findPairs(arr){
 	}
 	return options;
 }
+
+function computersTurnImpossible(){
+	var pickedCell;
+	if(game.usedCells.length === 9){
+		setTimeout(function(){alert("It's a tie!");}, 10);
+		return
+	}
+	//FIRST MOVE
+	if(game.usedCellsX.length === 1){
+		var firstXmove = game.usedCellsX[0];
+		if( firstXmove === "5"){
+			var options = ["1", "3", "7", "9"];
+			pickedCell = options.splice(pickRandomNumber(options.length), 1)[0];
+		}
+		if(firstXmove === "1" || firstXmove === "3" || firstXmove === "7" || firstXmove === "9"){
+			pickedCell = "5";
+		}
+		if(firstXmove === "2" || firstXmove === "4" || firstXmove === "6" || firstXmove === "8"){
+			var options = [];
+			for (var i=0; i < 8; i++){
+				if(checkAvailability(game.winOptions[i], firstXmove)){
+					for(var j=0; j < 3; j++){
+						if(game.winOptions[i][j] !== firstXmove){
+							options.push(game.winOptions[i][j]);
+						}
+					}
+				}
+			}
+			pickedCell = options.splice(pickRandomNumber(options.length), 1)[0];
+		}
+	}
+	//END FIRST MOVE
+	if(game.usedCellsX.length >= 2){
+		//CHECK IF X HAS 2 X'S IN THE SAME ROW
+		var pairs = findPairs(game.usedCellsX);
+		for(var j=0; j< pairs.length; j++){
+			for (var i=0; i < 8; i++){
+				if(checkAvailability(game.winOptions[i], pairs[j][0]) && checkAvailability(game.winOptions[i], pairs[j][1])){
+					for(var k=0; k < 3; k++){
+						if(game.winOptions[i][k] !== pairs[j][0] && game.winOptions[i][k] !== pairs[j][1]){
+							if(checkAvailability(game.openCells, game.winOptions[i][k])){
+								pickedCell = game.winOptions[i][k];
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	// IF X DOESN'T HAVE 2 XX IN SAME ROW
+	// Find O spot on open winning Line
+	if(!pickedCell){
+		var options = [];
+		var options2 = [];
+		for(var p=0; p< game.usedCellsO.length; p++){
+			var moveO = game.usedCellsO[p];
+			for(var i=0; i< 6; i++){
+				if(checkAvailability(game.winOptions[i], moveO)){
+					options.push(game.winOptions[i]);		
+				}
+			}
+		}
+		for(var i=0; i< options.length; i++){
+			for(var j=0; j< game.usedCellsX.length; j++){
+				if(checkAvailability(options[i], game.usedCellsX[j])){
+					options.splice(i,1);
+				}
+			}
+		}
+		for(var i=0; i<options.length; i++){
+			for(var j=0; j<options[i].length; j++){
+				if(checkAvailability(game.openCells, options[i][j])){
+					options2.push(options[i][j])
+				}
+			}
+		}
+		pickedCell = options2[pickRandomNumber(options2.length)];
+	}
+	//CHECK IF O CAN WIN
+	if(game.usedCellsO.length >= 2){
+		var pairs = findPairs(game.usedCellsO);
+		for(var j=0; j< pairs.length; j++){
+			for (var i=0; i < 8; i++){
+				if(checkAvailability(game.winOptions[i], pairs[j][0]) && checkAvailability(game.winOptions[i], pairs[j][1])){
+					for(var k=0; k < 3; k++){
+						if(game.winOptions[i][k] !== pairs[j][0] && game.winOptions[i][k] !== pairs[j][1]){
+							if(checkAvailability(game.openCells, game.winOptions[i][k])){
+								pickedCell = game.winOptions[i][k];
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	//MAKE MOVE
+	game.openCells.splice(game.openCells.indexOf(pickedCell), 1);
+	makeComputerMove(pickedCell);
+}
+
+// COMPUTERS TURN HARD
+function computersTurnHard(){
+	var pickedCell;
+	if(game.usedCells.length === 9){
+		setTimeout(function(){alert("It's a tie!");}, 10);
+		return
+	}
+	//FIRST MOVE
+	if(game.usedCellsX.length === 1){
+		var firstXmove = game.usedCellsX[0];
+		if( firstXmove === "5"){
+			var options = ["1", "3", "7", "9"];
+			pickedCell = options.splice(pickRandomNumber(options.length), 1)[0];
+		}
+		if(firstXmove === "1" || firstXmove === "3" || firstXmove === "7" || firstXmove === "9"){
+			pickedCell = "5";
+		}
+		if(firstXmove === "2" || firstXmove === "4" || firstXmove === "6" || firstXmove === "8"){
+			var options = [];
+			for (var i=0; i < 8; i++){
+				if(checkAvailability(game.winOptions[i], firstXmove)){
+					for(var j=0; j < 3; j++){
+						if(game.winOptions[i][j] !== firstXmove){
+							options.push(game.winOptions[i][j]);
+						}
+					}
+				}
+			}
+			pickedCell = options.splice(pickRandomNumber(options.length), 1)[0];
+		}
+	}
+	//END FIRST MOVE
+	if(game.usedCellsX.length >= 2){
+		//CHECK IF X HAS 2 X'S IN THE SAME ROW
+		var pairs = findPairs(game.usedCellsX);
+		for(var j=0; j< pairs.length; j++){
+			for (var i=0; i < 8; i++){
+				if(checkAvailability(game.winOptions[i], pairs[j][0]) && checkAvailability(game.winOptions[i], pairs[j][1])){
+					for(var k=0; k < 3; k++){
+						if(game.winOptions[i][k] !== pairs[j][0] && game.winOptions[i][k] !== pairs[j][1]){
+							if(checkAvailability(game.openCells, game.winOptions[i][k])){
+								pickedCell = game.winOptions[i][k];
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	if(!pickedCell){
+		pickedCell = game.openCells.splice(pickRandomNumber(game.openCells.length), 1)[0];
+	}
+	//CHECK IF O CAN WIN
+	if(game.usedCellsO.length >= 2){
+		var pairs = findPairs(game.usedCellsO);
+		for(var j=0; j< pairs.length; j++){
+			for (var i=0; i < 8; i++){
+				if(checkAvailability(game.winOptions[i], pairs[j][0]) && checkAvailability(game.winOptions[i], pairs[j][1])){
+					for(var k=0; k < 3; k++){
+						if(game.winOptions[i][k] !== pairs[j][0] && game.winOptions[i][k] !== pairs[j][1]){
+							if(checkAvailability(game.openCells, game.winOptions[i][k])){
+								pickedCell = game.winOptions[i][k];
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	//MAKE MOVE
+	game.openCells.splice(game.openCells.indexOf(pickedCell), 1);
+	makeComputerMove(pickedCell);
+}
+
+// ******* RANDOM COMPUTER EASY *************
+function computersTurnEasy(){
+	if(game.usedCells.length === 9){
+		setTimeout(function(){alert("It's a tie!");}, 10);
+		return;
+	}
+	randomCell = game.openCells.splice(pickRandomNumber(game.openCells.length), 1)[0];
+	makeComputerMove(randomCell)
+}
+// TOGGLE PLAYER
+$('.toggle').click(function(e) {
+  var toggle = this;
+  
+  e.preventDefault();
+  
+  $(toggle).toggleClass('toggle--on')
+         .toggleClass('toggle--off')
+         .addClass('toggle--moving');
+  
+  setTimeout(function() {
+    $(toggle).removeClass('toggle--moving');
+  }, 200)
+});
+
+// DIFFICULTY GAME
+
+
+$('.placeholder').on('click', function (ev) {
+  $('.placeholder').css('opacity', '0');
+  $('.list__ul').toggle();
+});
+
+$('.list__ul a').on('click', function (ev) {
+   ev.preventDefault();
+   var index = $(this).parent().index();
+   $('.placeholder').text( $(this).text() ).css('opacity', '1');
+   game.difficulty = $(this).text()
+   $('.list__ul').find('li').eq(index).prependTo('.list__ul');
+   $('.list__ul').toggle();   
+   $( ".playagain" ).trigger( "click" );
+});
+
